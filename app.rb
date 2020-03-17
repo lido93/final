17c56b345a424c75339959b9157d32a5cfc17bb8
lido93 <lisa.dong@kellogg.northwestern.edu>
2020-03-17 18:19:28 +0000

    users

--- app.rb
+++ app.rb
@@ -16,7 +16,11 @@ after { puts; }                                                                 
 
 places_table = DB.from(:places)
 comments_table = DB.from(:comments)
-# users_table = DB.from(:users)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
 
 get "/" do
     puts places_table.all
@@ -27,7 +31,7 @@ end
 get "/places/:id" do
     @place = places_table.where(id: params[:id]).to_a[0]
     @comments = comments_table.where(place_id: @place[:id])
-    # @users_table = users_table
+    @users_table = users_table
     view "place"
 end
 
@@ -42,4 +46,37 @@ get "/places/:id/comments/create" do
     comments_table.insert(place_id: params["id"],
                        comments: params["comments"])
     view "create_comment"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do
+    puts params
+    hashed_password = BCrypt::Password.create(params["password"])
+    users_table.insert(name: params["name"], email: params["email"], password: hashed_password)
+    view "create_user"
+end
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    user = users_table.where(email: params["email"]).to_a[0]
+    puts BCrypt::Password::new(user[:password])
+    if user && BCrypt::Password::new(user[:password]) == params["password"]
+        session["user_id"] = user[:id]
+        @current_user = user
+        view "create_login"
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    @current_user = nil
+    view "logout"
 end
